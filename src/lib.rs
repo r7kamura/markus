3af14e660335a589a9574b3f38e8faaa469c1611
Ryo Nakamura <r7kamura@gmail.com>
2021-11-22 20:43:55 +0900

    Make tests more strict to commonmark spec

--- src/lib.rs
+++ src/lib.rs
@@ -10,296 +10,163 @@ doc_comment::doctest!("../README.md");
 #[cfg(test)]
 mod tests {
     use super::parser::Parser;
-    use super::types::Event::*;
-    use super::types::HeadingLevel::*;
-    use super::types::Tag::{Heading, Paragraph};
 
-    #[test]
-    fn parse_example_219_paragraphs() {
-        let text = include_str!("../tests/fixtures/markdowns/219.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("aaa\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("bbb\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn assert_parse(a: &str, b: &str) {
+        let parser = Parser::new(a);
+        let mut buffer = String::new();
+        crate::html::push_html(&mut buffer, parser);
+        assert_eq!(&buffer, b);
     }
 
     #[test]
-    fn parse_example_220_paragraphs() {
-        let text = include_str!("../tests/fixtures/markdowns/220.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("aaa\n")));
-        assert_eq!(parser.next(), Some(Text("bbb\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("ccc\n")));
-        assert_eq!(parser.next(), Some(Text("ddd\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_043() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/043.md"),
+            include_str!("../tests/fixtures/htmls/043.html"),
+        );
     }
 
     #[test]
-    fn parse_example_221_paragraphs() {
-        let text = include_str!("../tests/fixtures/markdowns/221.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("aaa\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("bbb\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_044() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/044.md"),
+            include_str!("../tests/fixtures/htmls/044.html"),
+        );
     }
 
     #[test]
-    fn parse_example_062_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/062.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Heading(H1))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H1))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H2))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H2))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H3))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H3))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H4))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H4))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H5))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H5))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H6))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H6))));
-        assert_eq!(parser.next(), None);
+    fn parse_example_045() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/045.md"),
+            include_str!("../tests/fixtures/htmls/045.html"),
+        );
     }
 
     #[test]
-    fn parse_example_063_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/063.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_046() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/046.md"),
+            include_str!("../tests/fixtures/htmls/046.html"),
+        );
     }
 
     #[test]
-    fn parse_example_064_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/064.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("#5 bolt\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("#hashtag\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_047() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/047.md"),
+            include_str!("../tests/fixtures/htmls/047.html"),
+        );
     }
 
     #[test]
-    fn parse_example_065_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/065.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_050() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/050.md"),
+            include_str!("../tests/fixtures/htmls/050.html"),
+        );
     }
 
     #[test]
-    fn parse_example_067_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/067.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Heading(H1))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H1))));
-        assert_eq!(parser.next(), None);
+    fn parse_example_051() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/051.md"),
+            include_str!("../tests/fixtures/htmls/051.html"),
+        );
     }
 
     #[test]
-    fn parse_example_068_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/068.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Heading(H3))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H3))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H2))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H2))));
-        assert_eq!(parser.next(), Some(Begin(Heading(H1))));
-        assert_eq!(parser.next(), Some(Text("foo")));
-        assert_eq!(parser.next(), Some(End(Heading(H1))));
-        assert_eq!(parser.next(), None);
+    fn parse_example_052() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/052.md"),
+            include_str!("../tests/fixtures/htmls/052.html"),
+        );
     }
 
     #[test]
-    fn parse_example_070_atx_headings() {
-        let text = include_str!("../tests/fixtures/markdowns/070.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("foo\n")));
-        assert_eq!(parser.next(), Some(Text("    # bar\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_053() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/053.md"),
+            include_str!("../tests/fixtures/htmls/053.html"),
+        );
     }
 
     #[test]
-    fn parse_example_043_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/043.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
+    fn parse_example_054() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/054.md"),
+            include_str!("../tests/fixtures/htmls/054.html"),
+        );
     }
 
     #[test]
-    fn parse_example_044_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/044.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_055() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/055.md"),
+            include_str!("../tests/fixtures/htmls/055.html"),
+        );
     }
 
     #[test]
-    fn parse_example_045_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/045.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_062() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/062.md"),
+            include_str!("../tests/fixtures/htmls/062.html"),
+        );
     }
 
     #[test]
-    fn parse_example_046_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/046.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("--\n")));
-        assert_eq!(parser.next(), Some(Text("**\n")));
-        assert_eq!(parser.next(), Some(Text("__\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_063() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/063.md"),
+            include_str!("../tests/fixtures/htmls/063.html"),
+        );
     }
 
     #[test]
-    fn parse_example_047_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/047.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
+    fn parse_example_064() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/064.md"),
+            include_str!("../tests/fixtures/htmls/064.html"),
+        );
     }
 
     #[test]
-    fn parse_example_048_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/048.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_067() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/067.md"),
+            include_str!("../tests/fixtures/htmls/067.html"),
+        );
     }
 
     #[test]
-    fn parse_example_049_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/049.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("Foo\n")));
-        assert_eq!(parser.next(), Some(Text("    ***\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_068() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/068.md"),
+            include_str!("../tests/fixtures/htmls/068.html"),
+        );
     }
 
     #[test]
-    fn parse_example_050_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/050.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
+    fn parse_example_219() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/219.md"),
+            include_str!("../tests/fixtures/htmls/219.html"),
+        );
     }
 
     #[test]
-    fn parse_example_051_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/051.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
+    fn parse_example_220() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/220.md"),
+            include_str!("../tests/fixtures/htmls/220.html"),
+        );
     }
 
     #[test]
-    fn parse_example_052_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/052.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
-    }
-
-    #[test]
-    fn parse_example_053_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/053.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
-    }
-
-    #[test]
-    fn parse_example_054_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/054.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), None);
-    }
-
-    #[test]
-    fn parse_example_055_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/055.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("_ _ _ _ a\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("a------\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("---a---\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
-    }
-
-    #[test]
-    fn parse_example_056_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/056.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text(text)));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
-    }
-
-    #[test]
-    fn parse_example_059_thematic_breaks() {
-        let text = include_str!("../tests/fixtures/markdowns/059.md");
-        let mut parser = Parser::new(text);
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("Foo\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), Some(ThematicBreak));
-        assert_eq!(parser.next(), Some(Begin(Paragraph)));
-        assert_eq!(parser.next(), Some(Text("bar\n")));
-        assert_eq!(parser.next(), Some(End(Paragraph)));
-        assert_eq!(parser.next(), None);
+    fn parse_example_221() {
+        assert_parse(
+            include_str!("../tests/fixtures/markdowns/221.md"),
+            include_str!("../tests/fixtures/htmls/221.html"),
+        );
     }
 }
